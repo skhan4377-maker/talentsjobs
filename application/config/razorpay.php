ddfa70ec9e2aaf25c898a6dce1eee23da96c93ba
Saifi Khan <skhan4377@gmail.com>
2026-07-26 22:24:23 +0530

    remove rozarypay id

--- application/config/razorpay.php
+++ application/config/razorpay.php
@@ -1,14 +1,14 @@
 <?php  defined('BASEPATH') OR exit('No direct script access allowed');
-//$config['razorpay_key_id'] = 'rzp_live_ioCwAGpkaet6le';
-//$config['razorpay_key_secret'] = '6rNQ6hhyKVJDXSIfy2o7vcEL';
+//$config['razorpay_key_id'] = '';
+//$config['razorpay_key_secret'] = '';
 
-$config['razorpay_key_id'] = 'rzp_test_EqqZaBOjRjHOcv';
-$config['razorpay_key_secret'] = 'ykDvBU0yTeJUloyDAYeGmYpV';
+$config['razorpay_key_id'] = '';
+$config['razorpay_key_secret'] = '';
 
 
 /**
  * Webhook secret (Dashboard se liya hua)
  */
-$config['razorpay_webhook_secret'] = 'rzp_webhook_refund_2026@Secure#Key';
+$config['razorpay_webhook_secret'] = '';
 
 ?>
